c24ef79dfbffeb73b3fbefebcc26ca9f99aa97db
Gkrumbach07 <gkrumbach@gmail.com>
2023-02-09 13:53:01 -0600

    render all data from postgres (not boxed)

--- lib/types/database.types.ts
+++ lib/types/database.types.ts
@@ -9,264 +9,138 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
-      projects: {
+      campsites: {
         Row: {
-          details: string | null
-          id: number
-          lastUpdated: string | null
-          link: string | null
-          pinned: boolean
-          status: Database["public"]["Enums"]["status"]
-          title: string
-        }
-        Insert: {
-          details?: string | null
-          id?: number
-          lastUpdated?: string | null
-          link?: string | null
-          pinned?: boolean
-          status: Database["public"]["Enums"]["status"]
-          title: string
-        }
-        Update: {
-          details?: string | null
-          id?: number
-          lastUpdated?: string | null
-          link?: string | null
-          pinned?: boolean
-          status?: Database["public"]["Enums"]["status"]
-          title?: string
-        }
-      }
-      projects_tags: {
-        Row: {
-          project_id: number
-          tag_id: string
-        }
-        Insert: {
-          project_id: number
-          tag_id: string
-        }
-        Update: {
-          project_id?: number
-          tag_id?: string
-        }
-      }
-      tags: {
-        Row: {
-          name: string
-        }
-        Insert: {
-          name: string
-        }
-        Update: {
-          name?: string
-        }
-      }
-      trails: {
-        Row: {
-          accessibil: string | null
-          admin_org: string | null
-          allowed_sn: string | null
-          allowed_te: string | null
-          attributes: string | null
-          atv_accpt_: string | null
-          atv_manage: string | null
-          atv_restri: string | null
-          bicycle_ac: string | null
-          bicycle_ma: string | null
-          bicycle_re: string | null
-          bmp: number | null
-          emp: number | null
-          fourwd_acc: string | null
-          fourwd_man: string | null
-          fourwd_res: string | null
+          cmt: string | null
+          desc: string | null
+          ele: number | null
           geom: unknown | null
-          gis_miles: number | null
-          hiker_pe_1: string | null
-          hiker_pe_2: string | null
-          hiker_pede: string | null
           id: number
-          managing_o: string | null
-          minimum_tr: string | null
-          motor_wa_1: string | null
-          motor_wa_2: string | null
-          motor_wate: string | null
-          motorcyc_1: string | null
-          motorcyc_2: string | null
-          motorcycle: string | null
-          mvum_symbo: number | null
-          national_t: number | null
-          nonmotor_1: string | null
-          nonmotor_2: string | null
-          nonmotor_w: string | null
-          objectid: number | null
-          pack_sad_1: string | null
-          pack_sad_2: string | null
-          pack_saddl: string | null
-          security_i: string | null
-          segment_le: number | null
-          shape_leng: number | null
-          snow_motor: string | null
-          snowmobi_1: string | null
-          snowmobi_2: string | null
-          snowmobile: string | null
-          snowshoe_a: string | null
-          snowshoe_m: string | null
-          snowshoe_r: string | null
-          special_mg: string | null
-          surface_fi: string | null
-          terra_base: string | null
-          terra_moto: string | null
-          trail_clas: string | null
-          trail_cn: string | null
-          trail_name: string | null
-          trail_no: string | null
-          trail_surf: string | null
-          trail_type: string | null
-          typical__1: string | null
-          typical__2: string | null
-          typical_tr: string | null
-          water_moto: string | null
-          xcountry_1: string | null
-          xcountry_2: string | null
-          xcountry_s: string | null
+          lake_id: number | null
+          lake_name: string | null
+          link1_href: string | null
+          name: string | null
+          rating: number | null
+          status: string | null
         }
         Insert: {
-          accessibil?: string | null
-          admin_org?: string | null
-          allowed_sn?: string | null
-          allowed_te?: string | null
-          attributes?: string | null
-          atv_accpt_?: string | null
-          atv_manage?: string | null
-          atv_restri?: string | null
-          bicycle_ac?: string | null
-          bicycle_ma?: string | null
-          bicycle_re?: string | null
-          bmp?: number | null
-          emp?: number | null
-          fourwd_acc?: string | null
-          fourwd_man?: string | null
-          fourwd_res?: string | null
+          cmt?: string | null
+          desc?: string | null
+          ele?: number | null
           geom?: unknown | null
-          gis_miles?: number | null
-          hiker_pe_1?: string | null
-          hiker_pe_2?: string | null
-          hiker_pede?: string | null
           id?: number
-          managing_o?: string | null
-          minimum_tr?: string | null
-          motor_wa_1?: string | null
-          motor_wa_2?: string | null
-          motor_wate?: string | null
-          motorcyc_1?: string | null
-          motorcyc_2?: string | null
-          motorcycle?: string | null
-          mvum_symbo?: number | null
-          national_t?: number | null
-          nonmotor_1?: string | null
-          nonmotor_2?: string | null
-          nonmotor_w?: string | null
-          objectid?: number | null
-          pack_sad_1?: string | null
-          pack_sad_2?: string | null
-          pack_saddl?: string | null
-          security_i?: string | null
-          segment_le?: number | null
-          shape_leng?: number | null
-          snow_motor?: string | null
-          snowmobi_1?: string | null
-          snowmobi_2?: string | null
-          snowmobile?: string | null
-          snowshoe_a?: string | null
-          snowshoe_m?: string | null
-          snowshoe_r?: string | null
-          special_mg?: string | null
-          surface_fi?: string | null
-          terra_base?: string | null
-          terra_moto?: string | null
-          trail_clas?: string | null
-          trail_cn?: string | null
-          trail_name?: string | null
-          trail_no?: string | null
-          trail_surf?: string | null
-          trail_type?: string | null
-          typical__1?: string | null
-          typical__2?: string | null
-          typical_tr?: string | null
-          water_moto?: string | null
-          xcountry_1?: string | null
-          xcountry_2?: string | null
-          xcountry_s?: string | null
+          lake_id?: number | null
+          lake_name?: string | null
+          link1_href?: string | null
+          name?: string | null
+          rating?: number | null
+          status?: string | null
         }
         Update: {
-          accessibil?: string | null
-          admin_org?: string | null
-          allowed_sn?: string | null
-          allowed_te?: string | null
-          attributes?: string | null
-          atv_accpt_?: string | null
-          atv_manage?: string | null
-          atv_restri?: string | null
-          bicycle_ac?: string | null
-          bicycle_ma?: string | null
-          bicycle_re?: string | null
-          bmp?: number | null
-          emp?: number | null
-          fourwd_acc?: string | null
-          fourwd_man?: string | null
-          fourwd_res?: string | null
+          cmt?: string | null
+          desc?: string | null
+          ele?: number | null
           geom?: unknown | null
-          gis_miles?: number | null
-          hiker_pe_1?: string | null
-          hiker_pe_2?: string | null
-          hiker_pede?: string | null
           id?: number
-          managing_o?: string | null
-          minimum_tr?: string | null
-          motor_wa_1?: string | null
-          motor_wa_2?: string | null
-          motor_wate?: string | null
-          motorcyc_1?: string | null
-          motorcyc_2?: string | null
-          motorcycle?: string | null
-          mvum_symbo?: number | null
-          national_t?: number | null
-          nonmotor_1?: string | null
-          nonmotor_2?: string | null
-          nonmotor_w?: string | null
-          objectid?: number | null
-          pack_sad_1?: string | null
-          pack_sad_2?: string | null
-          pack_saddl?: string | null
-          security_i?: string | null
-          segment_le?: number | null
-          shape_leng?: number | null
-          snow_motor?: string | null
-          snowmobi_1?: string | null
-          snowmobi_2?: string | null
-          snowmobile?: string | null
-          snowshoe_a?: string | null
-          snowshoe_m?: string | null
-          snowshoe_r?: string | null
-          special_mg?: string | null
-          surface_fi?: string | null
-          terra_base?: string | null
-          terra_moto?: string | null
-          trail_clas?: string | null
-          trail_cn?: string | null
-          trail_name?: string | null
-          trail_no?: string | null
-          trail_surf?: string | null
-          trail_type?: string | null
-          typical__1?: string | null
-          typical__2?: string | null
-          typical_tr?: string | null
-          water_moto?: string | null
-          xcountry_1?: string | null
-          xcountry_2?: string | null
-          xcountry_s?: string | null
+          lake_id?: number | null
+          lake_name?: string | null
+          link1_href?: string | null
+          name?: string | null
+          rating?: number | null
+          status?: string | null
+        }
+      }
+      lakes: {
+        Row: {
+          geom: unknown | null
+          id: number
+          name: string | null
+          osm_id: string | null
+        }
+        Insert: {
+          geom?: unknown | null
+          id?: number
+          name?: string | null
+          osm_id?: string | null
+        }
+        Update: {
+          geom?: unknown | null
+          id?: number
+          name?: string | null
+          osm_id?: string | null
+        }
+      }
+      portages: {
+        Row: {
+          cmt: string | null
+          desc: string | null
+          geom: unknown | null
+          id: number
+          link1_href: string | null
+          link1_text: string | null
+          link1_type: string | null
+          link2_href: string | null
+          link2_text: string | null
+          link2_type: string | null
+          name: string | null
+          number: number | null
+          src: string | null
+          type: string | null
+        }
+        Insert: {
+          cmt?: string | null
+          desc?: string | null
+          geom?: unknown | null
+          id?: number
+          link1_href?: string | null
+          link1_text?: string | null
+          link1_type?: string | null
+          link2_href?: string | null
+          link2_text?: string | null
+          link2_type?: string | null
+          name?: string | null
+          number?: number | null
+          src?: string | null
+          type?: string | null
+        }
+        Update: {
+          cmt?: string | null
+          desc?: string | null
+          geom?: unknown | null
+          id?: number
+          link1_href?: string | null
+          link1_text?: string | null
+          link1_type?: string | null
+          link2_href?: string | null
+          link2_text?: string | null
+          link2_type?: string | null
+          name?: string | null
+          number?: number | null
+          src?: string | null
+          type?: string | null
+        }
+      }
+      waterways: {
+        Row: {
+          geom: unknown | null
+          id: number
+          name: string | null
+          osm_id: string | null
+          waterway: string | null
+        }
+        Insert: {
+          geom?: unknown | null
+          id?: number
+          name?: string | null
+          osm_id?: string | null
+          waterway?: string | null
+        }
+        Update: {
+          geom?: unknown | null
+          id?: number
+          name?: string | null
+          osm_id?: string | null
+          waterway?: string | null
         }
       }
     }
@@ -274,17 +148,36 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
+      campsites: {
+        Args: Record<PropertyKey, never>
+        Returns: Record<string, unknown>[]
+      }
       install_available_extensions_and_test: {
         Args: Record<PropertyKey, never>
         Returns: boolean
       }
+      lakes: {
+        Args: Record<PropertyKey, never>
+        Returns: Record<string, unknown>[]
+      }
+      portages: {
+        Args: Record<PropertyKey, never>
+        Returns: Record<string, unknown>[]
+      }
       trails: {
+        Args: Record<PropertyKey, never>
+        Returns: Record<string, unknown>[]
+      }
+      waterways: {
         Args: Record<PropertyKey, never>
         Returns: Record<string, unknown>[]
       }
     }
     Enums: {
       status: "upcoming" | "active" | "inactive" | "done"
+    }
+    CompositeTypes: {
+      [_ in never]: never
     }
   }
 }
--- pages/bwca.tsx
+++ pages/bwca.tsx
@@ -6,23 +6,70 @@ import supabase from '../lib/api/supabase';
 
 type geoJSONType = {
     json_build_object: {
-        geometry: GeoJSON.Geometry
+        geometry: GeoJSON.Point
         id: number,
         properties: any,
         type: any
     }
 }
 
- const Bwca = () => {
-    const [geojson, setGeojson] = React.useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>({
+const Bwca = () => {
+    const [geojson_waterways, setGeojson_waterways] = React.useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>({
+        type: 'FeatureCollection',
+        features: []
+    })
+    const [geojson_lakes, setGeojson_lakes] = React.useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>({
+        type: 'FeatureCollection',
+        features: []
+    })
+    const [geojson_campsites, setGeojson_campsites] = React.useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>({
+        type: 'FeatureCollection',
+        features: []
+    })
+    const [geojson_portages, setGeojson_portages] = React.useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>({
         type: 'FeatureCollection',
         features: []
     })
     React.useEffect(() => {
-        supabase.rpc('trails').then(resp => {
+        supabase.rpc('campsites').then(resp => {
             const data = resp?.data as unknown as geoJSONType[]
-            console.log(resp?.data?.length, resp)
-            setGeojson(
+            setGeojson_campsites(
+                {
+                    type: 'FeatureCollection',
+                    features: [
+                        ...data.map(line => line.json_build_object)
+                    ]
+                }
+            )
+        })
+
+        supabase.rpc('portages').then(resp => {
+            const data = resp?.data as unknown as geoJSONType[]
+            setGeojson_portages(
+                {
+                    type: 'FeatureCollection',
+                    features: [
+                        ...data.map(line => line.json_build_object)
+                    ]
+                }
+            )
+        })
+
+        supabase.rpc('lakes').then(resp => {
+            const data = resp?.data as unknown as geoJSONType[]
+            setGeojson_lakes(
+                {
+                    type: 'FeatureCollection',
+                    features: [
+                        ...data.map(line => line.json_build_object)
+                    ]
+                }
+            )
+        })
+
+        supabase.rpc('waterways').then(resp => {
+            const data = resp?.data as unknown as geoJSONType[]
+            setGeojson_waterways(
                 {
                     type: 'FeatureCollection',
                     features: [
@@ -32,8 +79,6 @@ type geoJSONType = {
             )
         })
     }, [])
-
-    console.log(geojson)
 
     return (
         <Map
@@ -46,8 +91,20 @@ type geoJSONType = {
             style={{ aspectRatio: "16 / 9" }}
             mapStyle="https://api.maptiler.com/maps/outdoor-v2/style.json?key=xwoervoKmfA3fyUtVroU"
         >
-            <Source id="my-data" type="geojson" data={geojson}>
-                <Layer type="line" id='wd'/>
+            <Source id="my-data" type="geojson" data={geojson_campsites}>
+                <Layer type="circle" paint={{
+                    'circle-radius': 5,
+                    'circle-color': '#007cbf'
+                }} id='campsites' />
+            </Source>
+            <Source type="geojson" data={geojson_waterways}>
+                <Layer type="line" id='waterways' />
+            </Source>
+            <Source type="geojson" data={geojson_lakes}>
+                <Layer type="line" id='lakes' />
+            </Source>
+            <Source type="geojson" data={geojson_portages}>
+                <Layer type="line" id='portages' />
             </Source>
         </Map>
     )
