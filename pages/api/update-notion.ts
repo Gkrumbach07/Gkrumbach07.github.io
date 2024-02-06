5f1a290d755a5b5b0a826cb54f40834620906cef
Gage Krumbach <gkrumbach@gmail.com>
2024-02-05 22:30:53 -0600

    Update user authentication logic

--- pages/api/update-notion.ts
+++ pages/api/update-notion.ts
@@ -46,35 +46,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		// Immediately respond to the client
 		res.status(200).json({ message: 'Valid request received, processing in background' });
 
-		// Background task
-		setImmediate(async () => {
-			try {
-				// Place your long-running tasks here
-				// For example, API calls, database updates, etc.
-				console.log('Background task is running');
+		// // Background task
+		// setImmediate(async () => {
+		// try {
+		// Place your long-running tasks here
+		// For example, API calls, database updates, etc.
+		// console.log('Background task is running');
 
-				const { data } = await axios.get(url);
+		const { data } = await axios.get(url);
 
-				const $ = load(data);
-				const metaContentTitle = $(`meta[property='og:title']`).attr('content');
-				const metaContentDescription = $(`meta[property='og:description']`).attr('content');
+		const $ = load(data);
+		const metaContentTitle = $(`meta[property='og:title']`).attr('content');
+		const metaContentDescription = $(`meta[property='og:description']`).attr('content');
 
-				if (!metaContentTitle || !metaContentDescription) {
-					// return res.status(400).json({ message: 'Failed to parse meta data', error: true });
-					console.error('Failed to parse meta data');
-					return;
-				}
+		if (!metaContentTitle || !metaContentDescription) {
+			return res.status(400).json({ message: 'Failed to parse meta data', error: true });
+			// console.error('Failed to parse meta data');
+			// return;
+		}
 
-				const title = `${metaContentTitle}. ${metaContentDescription}`
+		const title = `${metaContentTitle}. ${metaContentDescription}`
 
-				console.log('Restaurant og:title from URL:', title);
+		console.log('Restaurant og:title from URL:', title);
 
-				const notion = new Client({ auth: process.env.NOTION_RESTAURANT_API_KEY });
-				const openai = new OpenAI({
-					apiKey: process.env.OPENAI_API_KEY,
-				});
+		const notion = new Client({ auth: process.env.NOTION_RESTAURANT_API_KEY });
+		const openai = new OpenAI({
+			apiKey: process.env.OPENAI_API_KEY,
+		});
 
-				const prompt = `Task: Create a JSON blob for restaurant details.
+		const prompt = `Task: Create a JSON blob for restaurant details.
 				- Format the output as JSON.
 				- Key: "restaurants", Value: Array of objects.
 				- Each object should have:
@@ -88,196 +88,196 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 				- If information for a key is unavailable, set its value to null.
 				TEXT: ${title}`
 
-				console.log('OpenAI prompt:', prompt)
+		console.log('OpenAI prompt:', prompt)
 
-				// Use OpenAI to parse the text
-				const openAiResponse = await openai.chat.completions.create({
-					response_format: {
-						type: "json_object"
-					},
-					messages: [{
-						role: "user",
-						content: prompt
-					}],
-					model: "gpt-4",
-				});
-
-				// Simplified parsing logic here - you would extract specifics like name, type of food, etc.
-				const message = openAiResponse.choices[0].message.content
-
-				if (!message) {
-					// return res.status(400).json({ message: 'Failed to parse data', error: true });
-					console.error('Failed to parse data');
-					return;
-				}
-
-				console.log('OpenAI response:', message);
-				let messageData: MessageData = { restaurants: [] };
-				try {
-					messageData = JSON.parse(message);
-				}
-				catch (error) {
-					console.error('Failed to parse OpenAI response:', error);
-					// try to find handles
-					const handles = message.match(/@([a-zA-Z0-9_]+)/g)
-					if (handles) {
-						messageData = {
-							restaurants: handles.map((handle) => ({ handle, name: handle }))
-						}
-					}
-				}
-
-				// validate the parsed data
-				if (!Array.isArray(messageData.restaurants)) {
-					// return res.status(400).json({ message: 'Invalid data format', error: true });
-					console.error('Invalid data format');
-					return;
-				}
-
-				const database_id = process.env.NOTION_RESTAURANT_DATABASE_ID;
-
-				if (!database_id) {
-					// return res.status(400).json({ message: 'Database ID is required', error: true });
-					console.error('Database ID is required');
-					return;
-				}
-
-				const loggedRestaurants = await notion.databases.query({ database_id });
-				// get all handles
-				const handles = loggedRestaurants.results.map((restaurant) => ((restaurant as PageObjectResponse)?.properties?.Handle as PageObjectResponse["properties"][string] & { type: "rich_text" })?.rich_text?.[0]?.plain_text)
-
-				// Update Notion Database
-				const added: string[] = []
-				const failed: string[] = []
-				const upvoted: string[] = []
-				await Promise.all(messageData.restaurants.map(async (restaurant) => {
-					const { name, handle, type, occasion, price, location, notes } = restaurant;
-
-					// Validate required fields
-					if (!name || !handle) {
-						console.error('Missing required fields:', restaurant);
-						failed.push(name || 'Unknown');
-						return;
-					}
-
-					// Construct the handle link outside the conditionals as it's used in both new creations and updates
-					const handleLink = `https://instagram.com/${handle.slice(1)}`;
-
-					// Check if the restaurant already exists by handle
-					const existingRestaurantIndex = handles.findIndex(existingHandle => existingHandle === handle);
-					if (existingRestaurantIndex !== -1) {
-						// Existing restaurant found, get its page ID
-						const existingPageId = loggedRestaurants.results[existingRestaurantIndex].id;
-
-						try {
-							// Increment the count by retrieving the current value and adding 1
-							const currentCount = ((loggedRestaurants.results[existingRestaurantIndex] as PageObjectResponse).properties.Count as any)?.number || 0;
-							await notion.pages.update({
-								page_id: existingPageId,
-								properties: {
-									Count: { number: currentCount + 1 },
-								},
-							});
-
-							// Append a new text block with the notes if notes exist
-							if (notes) {
-								await notion.blocks.children.append({
-									block_id: existingPageId,
-									children: [
-										{
-											object: 'block',
-											type: 'paragraph',
-											paragraph: {
-												rich_text: [{ type: "text", text: { content: notes } }],
-											},
-										},
-									],
-								});
-							}
-							if (personalNote) {
-								await notion.blocks.children.append({
-									block_id: existingPageId,
-									children: [
-										{
-											object: 'block',
-											type: 'quote',
-											quote: {
-												rich_text: [{ type: "text", text: { content: personalNote } }],
-											},
-										},
-									],
-								});
-							}
-
-							upvoted.push(name); // Assuming 'upvoted' means handled or updated in this context
-						} catch (error) {
-							console.error('Failed to update existing restaurant:', name, error);
-							failed.push(name);
-						}
-					} else {
-						// No existing restaurant found, proceed to create a new one
-						const blocks: Array<BlockObjectRequest> = []
-						if (notes) {
-							blocks.push({
-								object: 'block',
-								type: 'paragraph',
-								paragraph: {
-									rich_text: [{ type: 'text', text: { content: notes } }],
-								},
-							})
-						}
-						if (personalNote) {
-							blocks.push({
-								object: 'block',
-								type: 'quote',
-								quote: {
-									rich_text: [{ type: 'text', text: { content: personalNote } }],
-								},
-							})
-						}
-						try {
-							await notion.pages.create({
-								parent: { database_id },
-								properties: {
-									Name: { title: [{ text: { content: name } }] },
-									Handle: { rich_text: [{ text: { content: handle, link: { url: handleLink } } }] },
-									URL: { url },
-									...(type ? { Type: { rich_text: [{ text: { content: type } }] } } : {}),
-									...(occasion ? { Occasion: { multi_select: occasion.map((o) => ({ name: o })) } } : {}),
-									...(price ? { Price: { number: price } } : {}),
-									...(location ? { Location: { rich_text: [{ text: { content: location } }] } } : {}),
-									Count: { number: 1 }, // Initialize count for new restaurant
-								},
-								children: blocks
-							});
-
-							added.push(name);
-						} catch (error) {
-							console.error('Failed to create new restaurant:', name, error);
-							failed.push(name);
-						}
-					}
-				}));
-
-				const respMessageArray = []
-				if (added.length > 0) {
-					respMessageArray.push(`Added: ${added.join(', ')}`)
-				}
-				if (upvoted.length > 0) {
-					respMessageArray.push(`Upvoted: ${upvoted.join(', ')}`)
-				}
-				if (failed.length > 0) {
-					respMessageArray.push(`Failed: ${failed.join(', ')}`)
-				}
-				console.log('Notion database updated:', respMessageArray.join(' | '));
-				// res.status(200).json({ message: respMessageArray.join(' | '), error: false });
-
-				console.log('Background task completed');
-			} catch (error) {
-				console.error('Background task failed:', error);
-				// Implement error handling or logging as needed
-			}
+		// Use OpenAI to parse the text
+		const openAiResponse = await openai.chat.completions.create({
+			response_format: {
+				type: "json_object"
+			},
+			messages: [{
+				role: "user",
+				content: prompt
+			}],
+			model: "gpt-4",
 		});
+
+		// Simplified parsing logic here - you would extract specifics like name, type of food, etc.
+		const message = openAiResponse.choices[0].message.content
+
+		if (!message) {
+			return res.status(400).json({ message: 'Failed to parse data', error: true });
+			// console.error('Failed to parse data');
+			// return;
+		}
+
+		console.log('OpenAI response:', message);
+		let messageData: MessageData = { restaurants: [] };
+		try {
+			messageData = JSON.parse(message);
+		}
+		catch (error) {
+			console.error('Failed to parse OpenAI response:', error);
+			// try to find handles
+			const handles = message.match(/@([a-zA-Z0-9_]+)/g)
+			if (handles) {
+				messageData = {
+					restaurants: handles.map((handle) => ({ handle, name: handle }))
+				}
+			}
+		}
+
+		// validate the parsed data
+		if (!Array.isArray(messageData.restaurants)) {
+			return res.status(400).json({ message: 'Invalid data format', error: true });
+			// console.error('Invalid data format');
+			// return;
+		}
+
+		const database_id = process.env.NOTION_RESTAURANT_DATABASE_ID;
+
+		if (!database_id) {
+			return res.status(400).json({ message: 'Database ID is required', error: true });
+			// console.error('Database ID is required');
+			// return;
+		}
+
+		const loggedRestaurants = await notion.databases.query({ database_id });
+		// get all handles
+		const handles = loggedRestaurants.results.map((restaurant) => ((restaurant as PageObjectResponse)?.properties?.Handle as PageObjectResponse["properties"][string] & { type: "rich_text" })?.rich_text?.[0]?.plain_text)
+
+		// Update Notion Database
+		const added: string[] = []
+		const failed: string[] = []
+		const upvoted: string[] = []
+		await Promise.all(messageData.restaurants.map(async (restaurant) => {
+			const { name, handle, type, occasion, price, location, notes } = restaurant;
+
+			// Validate required fields
+			if (!name || !handle) {
+				console.error('Missing required fields:', restaurant);
+				failed.push(name || 'Unknown');
+				return;
+			}
+
+			// Construct the handle link outside the conditionals as it's used in both new creations and updates
+			const handleLink = `https://instagram.com/${handle.slice(1)}`;
+
+			// Check if the restaurant already exists by handle
+			const existingRestaurantIndex = handles.findIndex(existingHandle => existingHandle === handle);
+			if (existingRestaurantIndex !== -1) {
+				// Existing restaurant found, get its page ID
+				const existingPageId = loggedRestaurants.results[existingRestaurantIndex].id;
+
+				try {
+					// Increment the count by retrieving the current value and adding 1
+					const currentCount = ((loggedRestaurants.results[existingRestaurantIndex] as PageObjectResponse).properties.Count as any)?.number || 0;
+					await notion.pages.update({
+						page_id: existingPageId,
+						properties: {
+							Count: { number: currentCount + 1 },
+						},
+					});
+
+					// Append a new text block with the notes if notes exist
+					if (notes) {
+						await notion.blocks.children.append({
+							block_id: existingPageId,
+							children: [
+								{
+									object: 'block',
+									type: 'paragraph',
+									paragraph: {
+										rich_text: [{ type: "text", text: { content: notes } }],
+									},
+								},
+							],
+						});
+					}
+					if (personalNote) {
+						await notion.blocks.children.append({
+							block_id: existingPageId,
+							children: [
+								{
+									object: 'block',
+									type: 'quote',
+									quote: {
+										rich_text: [{ type: "text", text: { content: personalNote } }],
+									},
+								},
+							],
+						});
+					}
+
+					upvoted.push(name); // Assuming 'upvoted' means handled or updated in this context
+				} catch (error) {
+					console.error('Failed to update existing restaurant:', name, error);
+					failed.push(name);
+				}
+			} else {
+				// No existing restaurant found, proceed to create a new one
+				const blocks: Array<BlockObjectRequest> = []
+				if (notes) {
+					blocks.push({
+						object: 'block',
+						type: 'paragraph',
+						paragraph: {
+							rich_text: [{ type: 'text', text: { content: notes } }],
+						},
+					})
+				}
+				if (personalNote) {
+					blocks.push({
+						object: 'block',
+						type: 'quote',
+						quote: {
+							rich_text: [{ type: 'text', text: { content: personalNote } }],
+						},
+					})
+				}
+				try {
+					await notion.pages.create({
+						parent: { database_id },
+						properties: {
+							Name: { title: [{ text: { content: name } }] },
+							Handle: { rich_text: [{ text: { content: handle, link: { url: handleLink } } }] },
+							URL: { url },
+							...(type ? { Type: { rich_text: [{ text: { content: type } }] } } : {}),
+							...(occasion ? { Occasion: { multi_select: occasion.map((o) => ({ name: o })) } } : {}),
+							...(price ? { Price: { number: price } } : {}),
+							...(location ? { Location: { rich_text: [{ text: { content: location } }] } } : {}),
+							Count: { number: 1 }, // Initialize count for new restaurant
+						},
+						children: blocks
+					});
+
+					added.push(name);
+				} catch (error) {
+					console.error('Failed to create new restaurant:', name, error);
+					failed.push(name);
+				}
+			}
+		}));
+
+		const respMessageArray = []
+		if (added.length > 0) {
+			respMessageArray.push(`Added: ${added.join(', ')}`)
+		}
+		if (upvoted.length > 0) {
+			respMessageArray.push(`Upvoted: ${upvoted.join(', ')}`)
+		}
+		if (failed.length > 0) {
+			respMessageArray.push(`Failed: ${failed.join(', ')}`)
+		}
+		console.log('Notion database updated:', respMessageArray.join(' | '));
+		// res.status(200).json({ message: respMessageArray.join(' | '), error: false });
+
+		// console.log('Background task completed');
+		// } catch (error) {
+		// 	console.error('Background task failed:', error);
+		// 	// Implement error handling or logging as needed
+		// }
+		// });
 	}
 	catch (error) {
 		console.error('Failed to update Notion database:', error);
